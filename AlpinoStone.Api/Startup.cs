baed3156c487145a05b408f65474b542ffed0eac
Zdenko <petraszdenko@gmail.com>
2019-09-10 23:38:06 +0200

    alpinostone project init

--- AlpinoStone.Api/Startup.cs
+++ AlpinoStone.Api/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AlpinoStone.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace AlpinoStone.Api
 {
@@ -25,7 +21,30 @@ namespace AlpinoStone.Api
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+			services.Configure<IISOptions>(options =>
+			{
+				options.ForwardClientCertificate = false;
+			});
+
+			services.AddDbContext<AlpinoContext>(options =>
+			{
+				options.UseSqlServer(Configuration.GetConnectionString("AlpinoDbConnection"));
+			});
+
+			services.AddCors();
+
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+			services.AddSwaggerGen(c =>
+				{
+					c.SwaggerDoc("v1", new OpenApiInfo
+					{
+						Version = "v1",
+						Title = "IO.Swagger",
+						Description = "IO.Swagger (ASP.NET Core 2.2)"
+					});
+					c.DescribeAllEnumsAsStrings();
+				});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +62,11 @@ namespace AlpinoStone.Api
 
 			app.UseHttpsRedirection();
 			app.UseMvc();
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "IO.Swagger");
+			});
 		}
 	}
 }
---
+++ AlpinoStone.Data/AlpinoContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AlpinoStone.Data
+{
+	public class AlpinoContext : DbContext
+	{
+		public AlpinoContext(DbContextOptions options) 
+			: base(options)
+		{
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=fsk-hp10;Database=AlpinoStone; Trusted_Connection= True;");
+		}
+
+	}
+}
